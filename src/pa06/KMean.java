9625dac2272476c465121e244b1558f70a139dee
SHIYI MAO <44790654+mao2021@users.noreply.github.com>
2020-04-18 00:35:43 -0400

    step 8

--- src/pa06/KMean.java
+++ src/pa06/KMean.java
@@ -9,43 +9,43 @@ import java.io.FileNotFoundException;
  * @author Yinghan Lin
  *
  */
-public class KMean {
+public class KMean{
 	Cluster[] clusters;
     Cluster originalData;
-	
+
 	public KMean(int k){
-		this.clusters = new Cluster[k]; 
-		this.originalData = new Cluster(); 
+		this.clusters = new Cluster[k];
+		this.originalData = new Cluster();
 	}
-	
+
 	public void setClusterPoint(int k) {
-		this.clusters = new Cluster[k];	
+		this.clusters = new Cluster[k];
 		for(int i=0; i<k; i++) {
 			clusters[i] = new Cluster();
 			clusters[i].setCluster(originalData.PickCluster(this.originalData.list));
-            
+
 			clusters[i].list = new ArrayList<Sample>();
 		}
 	}
-			
+
 	public static void main(String[]args) throws FileNotFoundException{
 		Scanner input = new Scanner(System.in);
 		System.out.println("Please enter the name of the file: ");
 		String filename = input.nextLine();
 		System.out.println("Please enter the number of clusters: ");
 		int k = input.nextInt();
-		input.close(); 
-		
+		input.close();
+
 		KMean kmean = new KMean(k);
-		ReadFile(filename, kmean);
-		kmean.originalData.PrintCluster();		
+		ReadFile(filename,kmean);//in test
+		kmean.originalData.PrintCluster();
 		kmean.setClusterPoint(k);
 		Reclassify(kmean.originalData.list, kmean.clusters);
 		for(int i=0; i<k; i++){
 			kmean.clusters[i].PrintCluster();
 		}
 	}
-	
+
 	public static void ReadFile(String filename, KMean kmean) throws FileNotFoundException {
 		File textfile = new File(filename);
 		Scanner sc = new Scanner(textfile);
@@ -55,12 +55,12 @@ public class KMean {
 			Sample point = new Sample(process(line));
 			point.setClusterId(i);
 			kmean.originalData.addSample(point);
-			i++; 
+			i++;
 		}
         sc.close();
-       
+
 	}
-		
+
 	public static double[] process(String line){
 		Scanner data = new Scanner(line);
         double[]values = new double[2];  //The length of the array values depends on the data set and can be modified.
@@ -68,19 +68,19 @@ public class KMean {
 		while(data.hasNextDouble()){
 			values[counter] = data.nextDouble();
 			counter++;
-		}			
+		}
 		data.close();
-		return values; 
+		return values;
 	}
-	
+
 	public static double Distance(Sample s1, Sample s2) {
 		double sum = 0;
 		for(int i=0; i<s1.size(); i++) {
-			sum = sum + Math.pow((s1.get(i)-s2.get(i)),2);   
+			sum = sum + Math.pow((s1.get(i)-s2.get(i)),2);
 		}
-		return Math.sqrt(sum); 
+		return Math.sqrt(sum);
 	}
-	
+
     public static int Closest(Sample point, Cluster[]clusters) {
 		double min = Distance(point, clusters[0].clusterPoint);
 		int count = 0;
@@ -88,13 +88,13 @@ public class KMean {
 			double d = Distance(point, clusters[j].clusterPoint);
 			if(d<=min) {
 				min = d;
-				
+
 				count = j;
 			}
 		}
 		return count;
 	}
-    
+
     public static void Reclassify(ArrayList<Sample> list, Cluster[]clusters) {
     	for(int i=0; i<list.size(); i++) {
     		int count = Closest(list.get(i), clusters);
@@ -102,10 +102,22 @@ public class KMean {
     		clusters[count].addSample(list.get(i));
     	}
     }
-	
-	//public static double[] FindAverage(Cluster cluster) {
-		//double[]average;
-		//return average; 
-	//}
-	
-}
+
+
+	public static double[] FindAverage(Cluster c) {
+		double sumX=0;
+		double sumY=0;
+		ArrayList<Sample> temp = c.getList();//getting the list of points in a cluster
+		int numOfPoints = temp.size();
+		for(int i=0;i<numOfPoints;i++){
+			Sample s = temp.get(i);
+			sumX+=s.get(0);
+			sumY+=s.get(1);
+		}//getting the sum of all x coordinates and y coordinates
+		double[] toReturn = new double[2];
+		toReturn[0] = sumX/numOfPoints;
+		toReturn[1] = sumY/numOfPoints;
+		return toReturn;
+	}
+
+}//end of the class
