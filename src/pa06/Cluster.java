f601e90fb73445b94fc7716fdf03e5548b4d50af
RXC0765 <63360122+RXC0765@users.noreply.github.com>
2020-04-18 20:46:33 -0400

    added comments

--- src/pa06/Cluster.java
+++ src/pa06/Cluster.java
@@ -10,34 +10,40 @@ import java.util.*;
  */
 
 public class Cluster {
-
+	 //an ArrayList (rows) of Samples and a single Sample (the cluster point)
 	 Sample clusterPoint;
-	 ArrayList<Sample> list; 
+	 ArrayList<Sample> list;
 	 
+	 //constructor, set the single Sample at {0.0,0.0}
 	 public Cluster() {
 		 double[] values = {0.0, 0.0};
 		 this.clusterPoint = new Sample(values);
 		 this.list = new ArrayList<Sample>();
 	 }
 	 
+	 //add a new point to the cluster
 	 public void addSample(Sample point) {
 		 list.add(point);
 	 } 
 	 
+	 //get the cluster list
 	 public ArrayList<Sample> getList() {
 		 return list;
 	 }
 	 
+	 //set the clusterPoint
 	 public void setCluster(Sample point) {
 		 this.clusterPoint = point;
 	 }
 	 
+	 //pick a random element of a cluster 
 	 public Sample PickCluster(ArrayList<Sample> list){
 		 Random rand = new Random();
 		 int randomNumber = rand.nextInt(list.size());
 		 return list.get(randomNumber);
 	 }
 	 
+	 //print cluster
 	 public void PrintCluster() {
 		 System.out.println("The Cluster Point: ");
 		 System.out.println(clusterPoint.toString());
